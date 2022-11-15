2969e96730b0cd44a71b9653f2d86b3dfa816c2e
Alfred Gaudin <gaudin.all@gmail.com>
2022-11-15 16:26:55 +0100

    la méthode create

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,4 +2,16 @@ class DocumentsController < ApplicationController
   def new
     @document = Document.new
   end
+
+  def create
+    @document = Document.new(document_params)
+    @document.save
+    redirect_to document_path(@document), status: :see_other
+  end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:url)
+  end
 end
