b7655919cdce6f63f33bee50e3182f4e9baf272d
Alfred Gaudin <gaudin.all@gmail.com>
2022-11-15 18:31:20 +0100

    le méthode show

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,6 @@
 class DocumentsController < ApplicationController
+  require "csv"
+
   def new
     @document = Document.new
   end
@@ -7,6 +9,14 @@ class DocumentsController < ApplicationController
     @document = Document.new(document_params)
     @document.save
     redirect_to document_path(@document), status: :see_other
+  end
+
+  def show
+    @document = Document.find(params[:id])
+    filepath = "/var/folders/11/zd366pys0hx8ndm2c2md8dtr0000gn/T/RackMultipart20221115-86792-7bejys.csv"
+    CSV.read("/var/folders/11/zd366pys0hx8ndm2c2md8dtr0000gn/T/RackMultipart20221115-86792-7bejys.csv")
+    table = CSV.parse(File.read("/var/folders/11/zd366pys0hx8ndm2c2md8dtr0000gn/T/RackMultipart20221115-86792-7bejys.csv"), headers: true, col_sep: ";")
+    raise
   end
 
   private
