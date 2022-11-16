ab9a3e4cef2fafa4bb51c515b0b35387fb50db46
Alfred Gaudin <gaudin.all@gmail.com>
2022-11-16 17:35:03 +0100

    les indicateurs sont visibles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_145936) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_150553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_145936) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "nbresa"
+    t.integer "nbbuyers"
+    t.integer "buyersage"
+    t.float "resaprice"
   end
 
 end
