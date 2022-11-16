4609d3e063a8d951b4017db36213e45c70e7ec63
Alfred Gaudin <gaudin.all@gmail.com>
2022-11-16 10:29:30 +0100

    visualisation de la base

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -8,15 +8,14 @@ class DocumentsController < ApplicationController
   def create
     @document = Document.new(document_params)
     @document.save
+    filepath = params["document"]["url"]
+    CSV.read(filepath)
+    table = CSV.parse(File.read(filepath), headers: true, col_sep: ";")
     redirect_to document_path(@document), status: :see_other
   end
 
   def show
-    @document = Document.find(params[:id])
-    filepath = "/var/folders/11/zd366pys0hx8ndm2c2md8dtr0000gn/T/RackMultipart20221115-86792-7bejys.csv"
-    CSV.read("/var/folders/11/zd366pys0hx8ndm2c2md8dtr0000gn/T/RackMultipart20221115-86792-7bejys.csv")
-    table = CSV.parse(File.read("/var/folders/11/zd366pys0hx8ndm2c2md8dtr0000gn/T/RackMultipart20221115-86792-7bejys.csv"), headers: true, col_sep: ";")
-    raise
+
   end
 
   private
